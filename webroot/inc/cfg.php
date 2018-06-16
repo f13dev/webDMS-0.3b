709c15324ee3ca2e2dcf9509ee90ee5df5212c80
toothless <toothless@toothless.local>
2018-06-16 19:40:27 +0100

    adding cfg basics

--- webroot/inc/cfg.php
+++ webroot/inc/cfg.php
@@ -4,7 +4,13 @@ webDMS configuration file, edit at your own risk
 **/
 
 // Database details
-define(DB_DATABASE,'webDMS');
-define(DB_USER,'webDMS');
-define(DB_PASSWORD,'webDMS');
-define(DB_HOST,'localhost');
+define(DB_DATABASE,'webDMS'); // Database name
+define(DB_USER,'webDMS'); // Database user
+define(DB_PASSWORD,'webDMS'); // Database users password
+define(DB_HOST,'localhost'); // Database host
+
+
+// Site structure
+define(SITE_WEBROOT,'/var/www/html/webDMS/webroot/'); // Path to webDMS
+define(SITE_DOCUMENTS,'/var/www/html/webDMS/webroot/documents/'); // Path to 'documents' (can be outside of web root)
+
