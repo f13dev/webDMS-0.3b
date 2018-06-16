41412f695e0cfba49586d47a73d8eb3dc9d17e53
toothless <toothless@toothless.local>
2018-06-16 19:35:30 +0100

    Start of config file

---
+++ webroot/inc/cfg.php
@@ -0,0 +1,10 @@
+<?php
+/**
+webDMS configuration file, edit at your own risk
+**/
+
+// Database details
+define(DB_DATABASE,'webDMS');
+define(DB_USER,'webDMS');
+define(DB_PASSWORD,'webDMS');
+define(DB_HOST,'localhost');
